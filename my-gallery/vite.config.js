e91f55fcc4fcd1720eaceb5389561fe220d20451
Efimover <126193267+Efimover@users.noreply.github.com>
2025-06-07 10:17:28 +0200

    Update vite.config.js

--- my-gallery/vite.config.js
+++ my-gallery/vite.config.js
@@ -2,7 +2,7 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  base: '/efimover.github.io/my-gallery/',
+  base: '/my-gallery/',
   plugins: [react()]
 })
 
